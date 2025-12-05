4796a38fca1108a3fbf7473c8badd468f8c2139b
AnaCastillo23 <89889778+AnaCastillo23@users.noreply.github.com>
2025-12-05 14:23:31 -0800

    Changed tab favicon and title

--- frontend/frontend/src/home.jsx
+++ frontend/frontend/src/home.jsx
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    document.title = "Crop Yield Predictor";
+
+    const link = document.querySelector("link[rel~='icon']");
+    if (link) {
+      link.href = "/crop.png";
+    }
+  }, []);
+
   return (
     <div>
-      <h1>Welcome to My Website</h1>
+      <h1>Crop Yield Predictor </h1>
       <p>This is the homepage</p>
     </div>
   );
