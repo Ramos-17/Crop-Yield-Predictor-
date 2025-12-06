07a505776deaf8e701579a276ada1a1036626cae
AnaCastillo23 <89889778+AnaCastillo23@users.noreply.github.com>
2025-12-05 20:58:05 -0800

    Finished logo header and added background

--- frontend/frontend/src/home.jsx
+++ frontend/frontend/src/home.jsx
@@ -3,7 +3,7 @@ import "./layout.css";
 
 export default function Home() {
   useEffect(() => {
-    document.title = "Crop Yield Predictor";
+    document.title = "Croply";
 
     const link = document.querySelector("link[rel~='icon']");
     if (link) {
@@ -12,14 +12,20 @@ export default function Home() {
   }, []);
 
   return (
-    <div>
-      <div className="top-header">
-        <div className="top-header-logo">
-            <h1>
-                Hello
-            </h1>
-            <p>hellooo</p>
-            
+    <div className="background-image">
+      <div className="background-tint">
+        <div className="top-header">
+          <div className="center-container">
+            <div className="logo-div-group">
+              <div className="logo-div">
+                <img className="logo" src="/crop.png" alt="Crop Logo" />
+              </div>
+              <div className="header-text-container">
+                <h1 className="header">Croply</h1>
+                <h2 className="subheader">Crop Prediction Made Easy</h2>
+              </div>
+            </div>            
+          </div>
         </div>
       </div>
     </div>
