02b32d403f9944263dd297d3af83b50b6cae0386
Ramos-17 <gramos0617@gmail.com>
2025-12-07 04:19:57 -0800

    Add .pkl files to backend

--- backend/app.py
+++ backend/app.py
@@ -2,27 +2,37 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
 import pandas as pd
+import zipfile
+import os
+
+MODEL_ZIP = "yield_random_forest.pkl.zip"
+MODEL_FILE = "yield_random_forest.pkl"
+
+# Unzip model if not extracted yet
+if not os.path.exists(MODEL_FILE) and os.path.exists(MODEL_ZIP):
+    with zipfile.ZipFile(MODEL_ZIP, "r") as zip_ref:
+        zip_ref.extractall("./")
 
 app = Flask(__name__)
-CORS(app)  # allow frontend to call this API
+CORS(app) #frontend to call backend
 
-# Load model and feature columns saved from Colab
-model = joblib.load("yield_random_forest.pkl")
-feature_columns = joblib.load("feature_columns.pkl")  # NOTE: .pkl, not .pk1
+# Load model and feature columns from Colab
+model = joblib.load(MODEL_FILE)
+feature_columns = joblib.load("feature_columns.pkl") 
 
 @app.route("/predict", methods=["POST"])
 def predict():
     # Get JSON from frontend
     data = request.get_json()
 
-    # Put JSON into a DataFrame (one row)
+    # Put JSON into DataFrame
     df = pd.DataFrame([data])
 
     # Handle avg_temp missing
     if "avg_temp" in df.columns:
         df["avg_temp"] = df["avg_temp"].fillna(df["avg_temp"].mean())
 
-    # One-hot encode Area and Item exactly like training
+    
     df = pd.get_dummies(df, columns=["Area", "Item"], drop_first=True)
 
     # Reindex to match training feature columns
